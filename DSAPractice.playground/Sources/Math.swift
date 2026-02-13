df12041d1e86f7d957d001456e553eef9277c1d3
Alexia Nunez <alexia@alexianunez.com>
2026-02-12 21:28:50 -0500

    Math.fizzBuzz

--- DSAPractice.playground/Sources/Math.swift
+++ DSAPractice.playground/Sources/Math.swift
@@ -65,6 +65,22 @@ public struct Math {
         return current
     }
     
+    public static func fizzBuzz(_ n: Int) -> [String] {
+        var answer: [String] = []
+        for i in 1...n {
+            if i % 15 == 0 {
+                answer.append("FizzBuzz")
+            } else if i % 3 == 0 {
+                answer.append("Fizz")
+            } else if i % 5 == 0 {
+                answer.append("Buzz")
+            } else {
+                answer.append("\(i)")
+            }
+        }
+        return answer
+    }
+    
     public static func tribonnaci(n: Int) -> Int {
         guard n > 2 else { return n }
         
